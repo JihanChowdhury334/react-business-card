0a27bd0b7f307953ba07e5441766a42efef797c4
jihan chowdhury <jihanchowdhury334@gmail.com>
2025-09-14 22:29:31 -0400

    Add React Business Card project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div className="card">
+    <div className="image">
+    </div>
+    <main>
+      <h1>Jihan Chowdhury</h1>
+      <h3>Frontend Developer</h3>
+      <h4>jihan.website</h4>
+
+      <div className="emli">
+      <span className="email">Email</span>
+      <span className="linkedin">Linkedin</span>
+      </div>
+
+      <h2>About</h2>
+      <p>im the goat</p>
+
+      <h2>Interests</h2>
+      <p>im the goat</p>
+    </main>
+    <footer>
+    <h5>social</h5>
+    <h5>social</h5>
+    <h5>social</h5>
+    <h5>social</h5>
+    
+    </footer>
+    </div>
+  )
+}
